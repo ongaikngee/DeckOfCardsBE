cb7ec972b66526e81b0e3cb4384a05f3c88e5b8f
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2026-06-15 14:54:25 +0800

    Tutorial: POST and PUT

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,17 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
-    
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -40,3 +46,20 @@ async def get_model(model_name: ModelName):
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item:Item, q:str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q":q})
+    return result
