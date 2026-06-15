7cdb79c0e0fd66d244e39f3e53534e552c3af681
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2026-06-15 08:36:23 +0800

    Tutorial: GET endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,15 @@
+from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+    
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/")
 def read_root():
@@ -16,3 +24,19 @@ def read_item(item_id: int, q: str | None = None):
 @app.get("/games/{game_id}")
 def read_game(game_id: int, q: str | None = None):
     return {"game_id": game_id, "q": q}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
