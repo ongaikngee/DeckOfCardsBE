32c098d6c6754852f60df632b8976667423181a5
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-24 16:08:35 +0800

    Feature: User can top up

--- src/app/models/chips.py
+++ src/app/models/chips.py
@@ -1,11 +1,13 @@
 from src.app.core.database import Base
-from sqlalchemy import Column, Integer, String  
+from sqlalchemy import Column, Integer, String, DateTime, func
+
 
 class Chips(Base):
-    __tablename__ = 'chips'
+    __tablename__ = "chips"
     id = Column(Integer, primary_key=True, index=True)
     amount = Column(Integer, nullable=False)
-    user_id = Column(Integer, foreign_key='users.id', nullable=False)
+    user_id = Column(Integer, foreign_key="users.id", nullable=False)
     reason = Column(String, nullable=True)
-    
-    
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
--- src/app/routers/chips.py
+++ src/app/routers/chips.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from datetime import datetime, timezone
 
 from src.app.core.database import SessionLocal
 from sqlalchemy.orm import Session
 from typing import Annotated
 from src.app.models.chips import Chips as ChipsModel
 from src.app.models.user import Users
+from enum import Enum
+from starlette import status
 
 router = APIRouter(prefix="/chips", tags=["Chips"])
+
 
 def get_db():
     db = SessionLocal()
@@ -15,46 +19,65 @@ def get_db():
         yield db
     finally:
         db.close()
-    
+
+
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class Reason(str, Enum):
+    topup = "Topup"
+    lottery = "Lottery"
+    jackpot = "Jackpot"
 
 
 class Chips(BaseModel):
     amount: int
-    user_id: int
-    reason: str | None = None
-    
-    
-@router.get("/")
-def get_all():
-    return {"data": [1,2,3,4]}
+    reason: Reason
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+def get_all(db: db_dependency, skip: int = 0, limit: int = 100):
+    chips = (
+        db.query(ChipsModel)
+        .order_by(ChipsModel.created_at.desc())
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+    return {"data": chips}
+
 
 @router.get("/{user_id}")
-def get_chips_by_user(user_id: int):
+def get_chips_by_user(user_id: int, db:db_dependency):
     # Check if user exists in users table, if not return error
-    db = next(get_db())
     user = db.query(Users).filter(Users.id == user_id).first()
     if not user:
-        return {"error": "User not found"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
     # Return chips for the user
     chips = db.query(ChipsModel).filter(ChipsModel.user_id == user_id).all()
     return {"data": chips}
 
-@router.post("/")
-def update_chips_count(chips: Chips, db: db_dependency):
+
+@router.post("/{user_id}", status_code=status.HTTP_201_CREATED)
+def update_chips_count(user_id: int, chips: Chips, db: db_dependency):
     chips_model = ChipsModel(
         amount=chips.amount,
-        user_id=chips.user_id,
-        reason=chips.reason
+        user_id=user_id,
+        reason=chips.reason,
+        created_at=datetime.now(timezone.utc),
     )
-    
+
     # check if user exists in users table, if not return error
-    user = db.query(Users).filter(Users.id == chips.user_id).first()
+    user = db.query(Users).filter(Users.id == user_id).first()
     if not user:
-        return {"error": "User not found"}
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+
     db.add(chips_model)
     db.commit()
     db.refresh(chips_model)
-    
+
     return {"data": chips_model.id}
